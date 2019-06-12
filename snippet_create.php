179f8044e561ee053d00b86cd92a989c4c1319c0
Antonin <antonin.guyot75@gmail.com>
2019-06-12 16:51:23 +0200

    added sandbox mode

--- snippet_create.php
+++ snippet_create.php
@@ -6,7 +6,7 @@ use Gateway\Gateway;
 
 $gateway = new Gateway;
 
-$gateway->setAppToken('xxxxxxxx');
+$gateway->setAppToken('UFG0EI5blBOFzg1aLf6sx32ovZt7z1sT54XUl4tfvHsYb6s6mW86pR3x7OuY');
 
 $payment = $gateway->create([
     "amount" => 50,
@@ -14,7 +14,8 @@ $payment = $gateway->create([
     "currency" => "EUR",
     "identifier" => "order-25",
     "descriptor" => "Achat test",
-    "processor" => "paypal"
+    "processor" => "paypal",
+    "sandbox" => True,
 ]);
 
 echo $payment->redirect_url;
