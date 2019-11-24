38518bcaf7810a4dd9f2c797fceee0d50702a23e
Antonin <antonin.guyot@icloud.com>
2019-11-24 13:03:25 +0000

    Apply fixes from StyleCI

--- examples/snippet_create.php
+++ examples/snippet_create.php
@@ -1,26 +1,26 @@
 <?php
 
-require __DIR__ . '../vendor/autoload.php';
+require __DIR__.'../vendor/autoload.php';
 
 use Ateros\Pay\Gateway;
 
-$gateway = new Gateway;
+$gateway = new Gateway();
 
 $gateway->setAppToken('');
 
 $payment = $gateway->create('payment', [
-    "amount" => 50,
-    "customer" => "customer@example.org",
-    "currency" => "EUR",
-    "identifier" => "order-25",
-    "descriptor" => "Achat test",
-    "processor" => "stripe",
-    "sandbox" => True,
+    'amount'     => 50,
+    'customer'   => 'customer@example.org',
+    'currency'   => 'EUR',
+    'identifier' => 'order-25',
+    'descriptor' => 'Achat test',
+    'processor'  => 'stripe',
+    'sandbox'    => true,
 ]);
 
-if ($payment->success){
+if ($payment->success) {
     echo $payment->data->redirect_url;
 } else {
-    echo "Error : " . PHP_EOL;
+    echo 'Error : '.PHP_EOL;
     var_dump($payment->errors);
 }
