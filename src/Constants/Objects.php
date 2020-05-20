26f2080a0111a09a22cda2d1d8dc74b9a99f2e41
Antonin <antonin.guyot@icloud.com>
2020-05-20 18:08:10 +0200

    fixed subscriptions

--- src/Constants/Objects.php
+++ src/Constants/Objects.php
@@ -3,8 +3,10 @@
 namespace Ateros\Pay\Constants;
 
 use Ateros\Pay\Objects\Payment;
+use Ateros\Pay\Objects\Subscription;
 
 class Objects
 {
     public static $payment = Payment::class;
+    public static $subscription = Subscription::class;
 }
